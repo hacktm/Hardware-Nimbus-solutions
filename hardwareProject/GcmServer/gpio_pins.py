a00d4675d1fd323be103ddbb90943895564ce0c8
Robert Tingirica <tingirica.robert@gmail.com>
2015-02-09 22:19:22 +0200

    replaced tabs with 4 spaces

--- hardwareProject/GcmServer/gpio_pins.py
+++ hardwareProject/GcmServer/gpio_pins.py
@@ -11,27 +11,27 @@ data = {'message':'You have a new visitor!', 'date': now.strftime('%c')}
 gerry = 'APA91bHzyoacjLlzbGzUIH3_asktUdZpmr86SUe9FcW-IcO5se3hLNLHWcaZZfYBu-25avhrhTpT5J280yzqfLrxQxEFT72f4bbg8G7xZn65weFXOzsyU2jBvuZwzit_qI6LasfImMqcCtwKSPbNlsDLE5poibcNlc0BF3hkNH1QnIrs8Bzztn4'
 
 def push_notification():
-	reg_ids = [gerry]
+    reg_ids = [gerry]
 
-	# response = gcm.json_request(registration_ids=reg_ids, data=data)
+    # response = gcm.json_request(registration_ids=reg_ids, data=data)
 
-	# Extra arguments
-	res = gcm.json_request(
-	    registration_ids=reg_ids, data=data,
-	    collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
-	)
+    # Extra arguments
+    res = gcm.json_request(
+        registration_ids=reg_ids, data=data,
+        collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
+    )
 
 
 GPIO.setmode(GPIO.BOARD)
 
 while True:
-	try:
-		GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-		GPIO.wait_for_edge(7, GPIO.RISING, 20)
-		push_notification()
-		print "sent"
-		time.sleep(1)
-		GPIO.cleanup()
-	except Exception as e:
-		raise e
+    try:
+        GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.wait_for_edge(7, GPIO.RISING, 20)
+        push_notification()
+        print "sent"
+        time.sleep(1)
+        GPIO.cleanup()
+    except Exception as e:
+        raise e
 
